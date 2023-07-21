56a427184a43321863fa82470c856444f10f3b61
Xiaoxing Cheng <hello@chengxiaoxing.me>
2023-07-21 15:32:24 +0800

    use ninja

--- configurations/default.sh
+++ configurations/default.sh
@@ -9,5 +9,5 @@ source_dir="$script_dir/../source"
 build_dir="$script_dir/../build/$config"
 install_dir="$script_dir/../install/$config"
 
-cmake -S $source_dir -B $build_dir -DCMAKE_INSTALL_PREFIX=$install_dir 
+cmake -S $source_dir -B $build_dir -DCMAKE_INSTALL_PREFIX=$install_dir -G Ninja
 cmake --build $build_dir --target install
